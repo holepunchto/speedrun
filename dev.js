e6345a3a1e90e44f8e862afe8a2ac4ce9190da6d
Rohan Chaudhary <rohanchaudhary.bkbiet2022@gmail.com>
2026-02-18 16:24:26 +0530

    updated a small syntax

--- dev.js
+++ dev.js
@@ -2,14 +2,14 @@ import updates from 'pear-updates'
 import { spawn } from 'bare-subprocess'
 import { isWindows } from '.'
 
-isWindows ? spawn('npm.cmd', ['/c', 'npm run build:dev'], { stdio: 'inherit' }) : spawn('npm', ['run', 'build:dev'], { stdio: 'inherit' })
+isWindows ? spawn('npm.cmd', ['run', 'build'], { stdio: 'inherit' }) : spawn('npm', ['run', 'build'], { stdio: 'inherit' })
 const REGULATE = 500
 let throttle = Date.now() + REGULATE
 updates({ app: true, version: { key: null } }, (update) => {
   if (Date.now() < throttle) return
   throttle = Infinity
   console.log('Update', update)
-  const build = isWindows ? spawn('npm.cmd', ['/c', 'npm run build:dev'], { stdio: 'inherit' }) : spawn('npm', ['run', 'build:dev'], { stdio: 'inherit' })
+  const build = isWindows ? spawn('npm.cmd', ['run', 'build'], { stdio: 'inherit' }) : spawn('npm', ['run', 'build'], { stdio: 'inherit' })
   build.on('close', () => {
     throttle = Date.now() + REGULATE
   })
